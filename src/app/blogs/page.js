1b6735214aab197bb3764613a342661dfe83c676
yash heda <yashheda5@outlook.com>
2024-10-29 10:45:33 +0530

    c7

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,21 +1,18 @@
 
 import React from 'react'
 import BlogOverview from '@/components/blog-overview'
-async function fetchListOfBlogs() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-
-  try {
-    const apiResponse = await fetch(`${baseUrl}/api/get-blog`, {
+async function fetchListOfBlogs(){
+  try{
+    const apiResponse =await fetch('http://localhost:3000/api/get-blog',{
       method: 'GET',
-      cache: 'no-store',
-    });
-    const result = await apiResponse.json();
-    return result?.data;
-  } catch (e) {
-    throw new Error(e.message);
+      cache :'no-store'
+    })
+    const result =await apiResponse.json();
+    return result?.data
+  } catch(e){
+    throw new Error(e)
   }
 }
-
 export default async function Blogs() {
   const blogList = await fetchListOfBlogs()
   console.log(blogList,'blogList')
