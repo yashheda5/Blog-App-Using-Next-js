3458dc834489e279e9320cdfc64750906f398715
yash heda <yashheda5@outlook.com>
2024-10-29 09:57:29 +0530

    c3

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,22 +1,22 @@
+import React from 'react';
+import BlogOverview from '@/components/blog-overview';
 
-import React from 'react'
-import BlogOverview from '@/components/blog-overview'
-async function fetchListOfBlogs(){
-  try{
-    const apiResponse =await fetch('http://localhost:3000/api/get-blog',{
+async function fetchListOfBlogs() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  try {
+    const apiResponse = await fetch(`${baseUrl}/api/get-blog`, {
       method: 'GET',
-      cache :'no-store'
-    })
-    const result =await apiResponse.json();
-    return result?.data
-  } catch(e){
-    throw new Error(e)
+      cache: 'no-store',
+    });
+    const result = await apiResponse.json();
+    return result?.data;
+  } catch (e) {
+    throw new Error(e);
   }
 }
+
 export default async function Blogs() {
-  const blogList = await fetchListOfBlogs()
-  console.log(blogList,'blogList')
-  return (
-   <BlogOverview blogList={blogList}/>
-  )
+  const blogList = await fetchListOfBlogs();
+  console.log(blogList, 'blogList');
+  return <BlogOverview blogList={blogList} />;
 }
